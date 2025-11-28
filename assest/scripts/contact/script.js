41e00d4c6821a15fa66113da02c456f32252c0f6
kasakigaming <minefc1111@gmail.com>
2025-11-29 01:52:05 +0700

    using hamburger for mobileMenu, addEventListener

---
+++ assest/scripts/contact/script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById('hamburger');
+    const mobileMenu = document.getElementById('mobileMenu');
+
+    hamburger.addEventListener('click', () => {
+        mobileMenu.classList.toggle('active');
+    });
+});
